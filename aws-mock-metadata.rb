1ae2886a44f52388068079c6ae6ca17e981303be
Cory Thomas <cory.thomas@bazaarvoice.com>
2014-04-24 14:14:42 -0500

    Added boto dependency

--- aws-mock-metadata.rb
+++ aws-mock-metadata.rb
@@ -5,4 +5,13 @@ class AwsMockMetadata < Formula
     head 'https://github.com/dump247/aws-mock-metadata.git'
 
     depends_on :python
+
+    resource 'boto' do
+        url 'https://pypi.python.org/packages/source/b/boto/boto-2.27.0.tar.gz'
+        sha1 '57277486b8d4be8dda3b86739148526a9afae05d'
+    end
+
+    def install
+        resource('boto').stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
+    end
 end
