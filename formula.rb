c7a45f1b4215acc3cb60f954d9472f0daf215a4c
Cory Thomas <cory.thomas@bazaarvoice.com>
2014-04-24 11:47:14 -0500

    Initial commit

---
+++ formula.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class AwsMockMetadata < Formula
+    homepage 'https://github.com/dump247/aws-mock-metadata'
+    head 'https://github.com/dump247/aws-mock-metadata.git'
+
+    depends_on :python
+end
